eab2320165d00c1320264c344e21e08fa2778d53
Md Mehedi Hasan <mh8399807@gmail.com>
2023-08-03 15:29:18 +0600

    Update ApiUrl.js

--- src/Local/ApiUrl.js
+++ src/Local/ApiUrl.js
@@ -1,5 +1,4 @@
-
-//const ApiUrl = 'https://server-food-app-two.vercel.app'
-const ApiUrl = 'http://localhost:5000'
+const ApiUrl = 'https://server-food-app-two.vercel.app'
+//const ApiUrl = 'http://localhost:5000'
 
 export default ApiUrl
